fdf01bcaf05072e95f553e288e3dcaf644565631
Abdullah Jasmin <84087327+AbdullahJasmin@users.noreply.github.com>
2022-09-04 23:01:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images"); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+ 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+ const upload = multer({ storage: fileStorageEngine });
+ 
+app.post("/fileUpload", upload.array("images", 3), (req, res) => {
+  console.log(req.files);
+  res.send("Multiple Files Upload Success");
+});
+
+app.listen(3000);
